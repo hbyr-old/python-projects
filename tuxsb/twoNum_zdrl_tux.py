1ee5e3ee8ddb008608c8ab5840795ea145f32779
hbyr-old <hb1426220475@163.com>
2024-11-26 16:35:31 +0800

    添加浏览器icon图标

---
+++ tuxsb/twoNum_zdrl_tux.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+from tuxsb import base
+
+url = '../images/twoNum_zdrl_images/'
+#单刷魂土
+for i in range(100):
+    # 3、进入魂土界面，配置御魂和开加成，随机20像素，延迟2-10s
+    for m in range(1, 6):
+        if m == 5:
+            base.one_click(url,f't_zdrl{m}.png', 20, random.randint(1, 2), 600, '0000')
+            #base.one_click(url, f't_zdrl{m}.png', 20, random.randint(1, 2), 10, '9998')
+        else:
+            base.one_click(url,f't_zdrl{m}.png', 20, random.randint(1, 2), 600, '0000')
+
+    print(f"============================{i}===========================================")
+    #time.sleep(random.randint(2, 30))
+
+
